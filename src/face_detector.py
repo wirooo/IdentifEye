9862a8f22a3bbae54b5d63a0dfd8532609016bcf
Will Lu <willh.lu@gmail.com>
2021-03-10 21:53:25 -0500

    update draw function

--- src/face_detector.py
+++ src/face_detector.py
@@ -13,20 +13,28 @@ class FaceDetector():
         self.mtcnn = mtcnn
         self.clf = clf.to(device)
         self.device = device
-        self.class_names = class_names
+        self.class_names = list(map(lambda str: str[0].upper() + str[1:].lower(), class_names))
 
-    def draw(self, frame, boxes, probs):
-        for box, prob in zip(boxes, probs):
-            cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), thickness=2)
-            cv2.putText(frame, f"face: {prob}", (box[2], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+    def draw(self, frame, boxes, face_probs, name_probs):
+        for box, face_prob, name_prob in zip(boxes, face_probs, name_probs):
+            name_preds = sorted([(self.class_names[i], name_prob[i]) for i in range(len(name_prob))], key=lambda x: x[1], reverse=True)
+            if len(name_preds) > 3:
+                name_preds = name_preds[:4]
+
+            cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), thickness=1)
+            cv2.putText(frame, f"face: {face_prob*100:.1f}%", (box[0], box[3]), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 1, cv2.LINE_AA)
+
+            for i, (name, pred) in enumerate(name_preds):
+                cv2.putText(frame, f"{name}: {pred:.3f}", (box[2], int(box[1] + i*25)), cv2.FONT_HERSHEY_SIMPLEX, 0.75 - (0.1*i), (0, 0, 255), 1, cv2.LINE_AA)
 
             return frame
+
 
     def detect_ROIs(self, boxes):
         return [[int(box[x]) for x in [1, 3, 0, 2]] for box in boxes]
 
 
-    def classify(self, face):
+    def classify_face(self, face):
         destRGB = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(destRGB.astype('uint8'), 'RGB')
 
@@ -45,22 +53,24 @@ class FaceDetector():
             _, pred = torch.max(out, 1)
 
         pred = np.array(pred[0].cpu())
-        return pred, self.class_names[pred]
+        out = np.array(out[0].cpu())
+        return pred, out
+
 
     def run(self):
         cap = cv2.VideoCapture(0)
         while True:
             ret, frame = cap.read()
-            boxes, probs = self.mtcnn.detect(frame)
-
-
-            ROIs = self.detect_ROIs(boxes)
-            for y1, y2, x1, x2 in ROIs:
+            boxes, face_probs = self.mtcnn.detect(frame)
+            name_probs = []
+            for box in boxes:
+                y1, y2, x1, x2 = int(box[1]), int(box[3]), int(box[0]), int(box[2])
                 face = frame[y1:y2, x1:x2]
-                pred, name = self.classify(face)
-                print(name)
+                pred, probs = self.classify_face(face)
+                name_probs.append(probs)
 
-            self.draw(frame, boxes, probs)
+            # self.draw(frame, boxes, face_probs)
+            self.draw(frame, boxes, face_probs, name_probs)
 
 
             cv2.imshow("Face Detection", frame)
